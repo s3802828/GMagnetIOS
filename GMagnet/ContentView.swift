3908acdb76f8948c0da8466149c9268472fa7f7e
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-09-01 11:31:11 +0700

    Initial Commit

---
+++ GMagnet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GMagnet
+//
+//  Created by Giang Le on 01/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GMagnet/GMagnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMagnetApp.swift
+//  GMagnet
+//
+//  Created by Giang Le on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GMagnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

33ba404a6f44e2bcc50e27e30a1b060eec192737
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-09-01 14:23:45 +0700

    auth backend

--- GMagnet/Auth/SignInView.swift
+++ GMagnet/Auth/SignInView.swift
@@ -6,10 +6,39 @@
 // Acknowledgement: https://blckbirds.com/post/user-authentication-with-swiftui-and-firebase/
 
 import SwiftUI
+import Firebase
 
 struct SignInView: View {
+    @State var signInProcessing = false
+    func signInUser(userEmail: String, userPassword: String) {
+        
+        signInProcessing = true
+        
+        Auth.auth().signIn(withEmail: userEmail, password: userPassword) { authResult, error in
+            
+            guard error == nil else {
+                signInProcessing = false
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            switch authResult {
+            case .none:
+                print("Could not sign in user.")
+                signInProcessing = false
+            case .some(_):
+                print("User signed in")
+                signInProcessing = false
+            }
+            
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            signInUser(userEmail: "quynhgiangle206@gmail.com", userPassword: "abc1234")
+        }) {
+            Capsule()
+                .overlay(Text("Sign Up"))
+        }
     }
 }
 

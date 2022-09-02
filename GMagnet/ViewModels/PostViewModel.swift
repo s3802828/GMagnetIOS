044b43aa80158f5631ac7c53ffa84e4137ef62d2
s3818245 <s3818245@rmit.edu.vn>
2022-09-02 16:17:19 +0700

    update post view model

---
+++ GMagnet/ViewModels/PostViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  PostViewModel.swift
+//  GMagnet
+//
+//  Created by Phan Anh on 02/09/2022.
+//
+
+import Foundation
+
+class PostViewModel: ObservableObject{
+    @Published var post: Post
+    @Published var comment_list: [Comment] = []
+    
+    init(post: Post){
+        self.post = post
+    }
+    
+    func fetch_comments(){
+        self.comment_list = self.post.comment_list
+    }
+    
+    func add_comment(added_comment: Comment){
+        Comment.add_comment(added_comment: added_comment)
+    }
+    
+    func update_comment(updated_comment: Comment){
+        Comment.update_comment(updated_comment: updated_comment)
+    }
+    
+    func delete_comment(deleted_comment: Comment){
+        Comment.delete_comment(deleted_comment: deleted_comment)
+    }
+}

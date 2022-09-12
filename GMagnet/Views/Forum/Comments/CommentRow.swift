da58c0d2161728e1111896d73cdccae9a2d1be2b
s3818520 <s3818520@rmit.edu.vn>
2022-09-13 00:51:26 +0700

    add button for edit/delete comment

--- GMagnet/Views/Forum/Comments/CommentRow.swift
+++ GMagnet/Views/Forum/Comments/CommentRow.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct CommentRow: View {
     var comment: Comment
+
+    @EnvironmentObject var currentUser: AuthenticateViewModel
+    
+    @State var isCommentEditing = false
+    
+    @State var commentEditInput = ""
+    
     var body: some View {
         HStack {
             VStack {
@@ -53,13 +60,81 @@ struct CommentRow: View {
                         .fontWeight(.bold)
                 }
                 
+                HStack {
+                    if (isCommentEditing) {
+                        HStack {
+                            
+                            TextField("", text: $commentEditInput)
+                            
+                            Spacer()
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(systemName: "paperplane")
+                                    .foregroundColor(Color("ButtonColor"))
+                                
+                            })
+                        }.padding(.all, 5)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke().opacity(0.5))
+                    } else {
+                    Text(comment.content)
+                        .padding(.all, 5)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke().opacity(0.5))
+                    }
+                    Spacer()
+                    if (currentUser.currentUser.id == comment.user.id) {
+                        if (isCommentEditing) {
+                            Button(action: {
+                                isCommentEditing.toggle()
+                            }, label: {
+                                Image(systemName: "x.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20)
+                                    .foregroundColor(.black)
+                            })
+                        } else {
+                            Menu(content: {
+
+                                Button(action: {
+                                    isCommentEditing.toggle()
+                                    print("Edit clicked")
+                                }, label: {
+                                    HStack {
+                                        Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                                            .foregroundColor(Color("ButtonColor"))
+                                        Text("Edit")
+                                    }
+                                })
+                                
+                                Button(action: {
+
+                                }, label: {
+                                    HStack {
+                                        Image(systemName: "trash")
+                                            .foregroundColor(Color("ButtonColor"))
+                                        Text("Delete")
+                                    }
+                                })
+                            }, label: {
+                                Image(systemName: "ellipsis.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20)
+                                    .foregroundColor(Color("ButtonColor"))
+                                    
+                            })
+                        }
+                    }
+                    
+                }
                 
-                Text(comment.content)
-                    .padding(.all, 5)
-                    .overlay(RoundedRectangle(cornerRadius: 5).stroke().opacity(0.5))
             }
         }
         .padding(.all, 3)
         .frame(width: 370, alignment: .leading)
+        .onAppear{
+            commentEditInput = comment.content
+        }
     }
 }
